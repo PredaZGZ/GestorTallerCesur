ab4fb434f0a3af63c66deaa9ea2f9c1807121367
pablo <pablo.s591516@cesurformacion.com>
2026-03-23 14:12:31 +0100

    feat: añado el constructor, los getters, setters y el toString

--- Item.java
+++ Item.java
@@ -2,4 +2,32 @@ package Taller;
 
 public class Item {
 
+    private String nombre;
+    private Double precio;
+
+    public Item (String nombre, Double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String matricula) {
+		this.nombre = nombre;
+	}
+
+    public Double getprecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+    @Override
+	public String toString() {
+		return "Nombre: " + this.nombre + " Precio: " + this.precio;
+	}
 }
