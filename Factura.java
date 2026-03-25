967f69359867bad03901260488793c2499b6292c
julia <julia.rus@gmail.com>
2026-03-25 11:02:44 +0100

    feat: Añadido getters y setters en la clase Factura

--- Factura.java
+++ Factura.java
@@ -6,4 +6,20 @@ public class Factura {
 	
 	private int id;
 	private ArrayList<Item> listaElementos;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public ArrayList<Item> getListaElementos() {
+		return listaElementos;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setListaElementos(ArrayList<Item> listaElementos) {
+		this.listaElementos = listaElementos;
+	}
 }
