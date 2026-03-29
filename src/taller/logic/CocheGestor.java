6b6c0cdcf54c8e4a60492d6a85c1f8d46c46ac36
Marcos Gracia <graciagarciamarcos@gmail.com>
2026-03-29 15:28:07 +0200

    refactor: Mover método addCoche a CocheGestor

--- src/taller/logic/CocheGestor.java
+++ src/taller/logic/CocheGestor.java
@@ -8,4 +8,17 @@ public class CocheGestor {
 
     ArrayList<Coche> listaCoches = new ArrayList<>();
 
+    public void addCoche(String matricula, String modelo, String descripcionFallo) {
+		for (Coche c : listaCoches) {
+			if (matricula.equals(c.getMatricula())) {
+				throw new IllegalArgumentException("Esta matricula ya esta añadida, añade otra");
+				
+			}
+		}
+		Coche coche = new Coche(matricula,modelo,descripcionFallo);
+			
+		listaCoches.add(coche);
+
+	}
+
 }
--- src/taller/logic/Taller.java
+++ src/taller/logic/Taller.java
@@ -4,17 +4,5 @@ import java.util.ArrayList;
 
 public class Taller {
 	
-	public void addCoche(String matricula, String modelo, String descripcionFallo) {
-		for (Coche c : listaCoches) {
-			if (matricula.equals(c.getMatricula())) {
-				throw new IllegalArgumentException("Esta matricula ya esta añadida, añade otra");
-				
-			}
-		}
-		Coche coche = new Coche(matricula,modelo,descripcionFallo);
-			
-		listaCoches.add(coche);
-
-	}
 	
 }
