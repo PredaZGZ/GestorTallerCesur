d28af58ce7becae464d6920d08718c225e0cb95e
adellmellajosemaria <adellmellajosemaria@gmail.com>
2026-04-01 16:55:55 +0200

    feat: Añadidos getters y setters en la clase Item

--- src/taller/Item.java
+++ src/taller/Item.java
@@ -1,5 +1,56 @@
 package taller;
 
-public class Item {
+    private final int stock;
 
+public class Item {
+    private int id;
+    private string nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    
+    public Item(int id, String nombre, String descripcion, double precio, int stock) {
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.stock = stock;
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
 }
