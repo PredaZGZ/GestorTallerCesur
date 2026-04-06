a041775a2f25ba9b781cb1033fd291aac3fdcd57
julia <julia.rus@gmail.com>
2026-04-06 19:04:05 +0200

    feat: Añadido método para contratar nuevos empleados al gestor

--- EmpleadoGestor.java
+++ EmpleadoGestor.java
@@ -4,4 +4,9 @@ public class EmpleadoGestor {
 
     ArrayList<Empleado> listaEmpleados = new ArrayList<>();
 
+    public void contratarEmpleado(String nombre, String puesto, double sueldo, String turno) {
+		Empleado nuevoEmpleado = new Empleado(nombre, puesto, sueldo, turno);
+		listaEmpleados.add(nuevoEmpleado);
+	}
+    
 }
