5be94ac7e277af3dd857afc13792d2f2493a950f
Taylimar <taylimar.s507500@cesurformacion.com>
2026-04-06 17:41:40 +0200

    feat: Coche_Interfaz en proceso

--- Coche_Interfaz.java
+++ Coche_Interfaz.java
@@ -8,9 +8,69 @@ public class Coche_Interfaz{
             opcionCo = sc.nextInt();
             System.out.println("2. Agrega un coche");
             opcionCo = sc.nextInt();
-            System.out.println("2. Buscar cliente por matricula");
+            System.out.println("3. Buscar cliente por matricula");
             opcionCo = sc.nextInt();
-            System.out.println("3. Eliminar cliente");
+            System.out.println("4. Actualizar fallos");
             opcionCo = sc.nextInt();
+
+
+            switch (opcionCo) {
+                case  1 -> {
+                    
+                }
+                case 2 -> {
+                   System.out.println("--- Agregar los datos del vehiculo ---");
+                    System.out.print("Indica el DNI del dueño para buscarlo: ");
+                    String dniBusqueda = sc.nextLine();
+    
+   
+                     Cliente dueño = miTaller.BuscarCliente(dniBusqueda);
+
+                     if (dueño == null) {
+
+                      System.out.println(" El cliente no existe. Debes crearlo primero en el menú de Clientes.");
+                      } else {
+                      System.out.print("Matricula del coche: ");
+                      String matricula = sc.nextLine();
+                     System.out.print("Modelo del coche: ");
+                     String modelo = sc.nextLine();
+                     System.out.print("Descripcion del fallo: ");
+                     String fallo = sc.nextLine();
+
+       
+                     miTaller.addCoche(dueño, matricula, modelo, fallo);
+                }
+            }
+            case 3 -> {
+             System.out.print("Matricula a buscar: ");
+            String matricula = sc.nextLine();
+            Coche encontrado = miTaller.BuscarCoche(matricula);
+            if(encontrado != null) {
+            System.out.println(encontrado);
+            }else{
+            System.out.println("No encontrado");
+
+            } 
+                
+            }
+            case 4 -> {
+                System.out.println("--- ACTUALIZAR INFORME DE FALLO ---");
+             System.out.print("Introduce la matrícula del vehículo: ");
+             String matricula = sc.nextLine();
+    
+    
+             Coche c = miTaller.BuscarCoche(matricula);
+    
+              if (c != null) {
+             System.out.println("Fallo actual: " + c.getDescripcionFallo());
+             System.out.print("Introduce la nueva descripción del fallo: ");
+              String nuevoFallo = sc.nextLine();
+        
+       
+              miTaller.actualizarFallo(matricula, nuevoFallo);
+             } else {
+                System.out.println("Error: Ese coche no está registrado en el sistema.");
+             }
+            }
     }
 }
